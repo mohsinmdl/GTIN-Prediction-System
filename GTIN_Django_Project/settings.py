f1afad326fab777bcb11e1d8aec214d4aaead90d
Mohsin Mahmood <mohsinmahmoodmdl@gmail.com>
2019-03-21 03:41:10 +0500

    fix staticroot error favicon

--- GTIN_Django_Project/settings.py
+++ GTIN_Django_Project/settings.py
@@ -126,13 +126,16 @@ USE_TZ = True
 #]
 
 
-PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
+
+
+
+
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
-)
 
 django_heroku.settings(locals())
