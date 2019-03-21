63d365b106c226add19bd1877f8f4471ac72d5ac
Mohsin Mahmood <mohsinmahmoodmdl@gmail.com>
2019-03-21 12:42:37 +0500

    majestic upload2

--- GTIN_Application/urls.py
+++ GTIN_Application/urls.py
@@ -15,11 +15,12 @@ Including another URLconf
 """
 
 from django.conf.urls import url
+from django.urls import path
 from . import views
 
 urlpatterns = [
-
-    url(r'^$', views.index, name='index'),
+	path('', views.index),
+    #url(r'^$', views.index, name='index'),
     url(r'^charts/$', views.charts, name='charts')
     # path('getdata/', views.index),
 
