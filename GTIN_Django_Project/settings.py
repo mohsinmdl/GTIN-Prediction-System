4eafa6869dff22bbd896ecb3b27282b45a901412
Mohsin Mahmood <mohsinmahmoodmdl@gmail.com>
2019-04-07 17:25:03 +0500

    Comment on Heroku use

--- GTIN_Django_Project/settings.py
+++ GTIN_Django_Project/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-import django_heroku
+# import django_heroku
 from decouple import config
 import dj_database_url
 
@@ -138,4 +138,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-django_heroku.settings(locals())
+#django_heroku.settings(locals())
